f7d98be0b9426037e80d579f69280a59a3f833c7
Antoni Poe Cruzado Pacheco <85760695+StrangeKode@users.noreply.github.com>
2025-09-18 14:18:42 -0500

    Delete Villain-of-Process-main directory

--- Villain-of-Process-main/src/Process.cpp
+++
@@ -1,8 +0,0 @@
-#include "Process.h"
-
-/*
- * Implementación del constructor de la clase Process.
- * Inicializa los atributos con los valores proporcionados.
- */
-Process::Process(int pid, const std::string& user, const std::string& group, const std::string& name, double cpu_usage, long mem, long disk_read, long disk_write, long net_sent, long net_recv, double gpu_usage, bool is_service)
-    : pid(pid), user(user), group(group), name(name), cpu_usage(cpu_usage), mem(mem), disk_read(disk_read), disk_write(disk_write), net_sent(net_sent), net_recv(net_recv), gpu_usage(gpu_usage), is_service(is_service) {}
